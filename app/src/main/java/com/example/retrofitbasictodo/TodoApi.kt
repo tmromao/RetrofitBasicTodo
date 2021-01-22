29c41e1777da150f82e729d6a2003f83afd98d2e
RomaoRecipes <tiagomromao@gmail.com>
2021-01-22 11:45:35 +0000

    Project completed

---
+++ app/src/main/java/com/example/retrofitbasictodo/TodoApi.kt
@@ -0,0 +1,11 @@
+package com.example.retrofitbasictodo
+
+import retrofit2.Response
+import retrofit2.http.GET
+
+interface TodoApi {
+
+    @GET("/todos")
+    suspend fun getTodos(): Response<List<Todo>>
+
+}
